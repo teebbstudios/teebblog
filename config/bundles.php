8728201cc89997a862bd4442e931208921f0203d
teebbstudios <qww.zone@gmail.com>
2021-06-22 19:36:06 +0800

    first page done

--- config/bundles.php
+++ config/bundles.php
@@ -2,4 +2,9 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
 ];
---
+++ src/Controller/TestController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TestController extends AbstractController
+{
+    #[Route('/test/{name}', name: 'test')]
+    public function index(Request $request, string $name ="Quan Weiwei"): Response
+    {
+//        $name = $request->attributes->get('name');
+        //dd($request);
+        //$page = $request->query->getInt('page', 1);
+        //$name = $request->query->get('name');
+
+        return new Response(<<<EOF
+<html>
+<head>
+<title>这是我们的第一个页面</title>
+</head>
+<body>
+<h1>$name</h1>
+</body>
+</html>
+EOF
+);
+    }
+}
