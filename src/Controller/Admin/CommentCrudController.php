9bf03d3b335a0a5bc831d6fc61952b091726c7d4
teebbstudios <qww.zone@gmail.com>
2021-06-29 18:41:55 +0800

    CommentCrudController

--- src/Controller/Admin/CommentCrudController.php
+++ src/Controller/Admin/CommentCrudController.php
@@ -3,7 +3,15 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Comment;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TimeField;
 
 class CommentCrudController extends AbstractCrudController
 {
@@ -12,14 +20,23 @@ class CommentCrudController extends AbstractCrudController
         return Comment::class;
     }
 
-    /*
+
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            IdField::new('id')->onlyOnIndex(),
+            TextField::new('author'),
+            EmailField::new('email'),
+            TextareaField::new('message'),
+            TimeField::new('createdAt')
+                ->setFormat('Y-MM-dd HH:mm:ss')->onlyOnIndex(),
+            TimeField::new('updatedAt')
+                ->setFormat('Y-MM-dd HH:mm:ss')->onlyOnIndex(),
         ];
     }
-    */
+
+    public function configureActions(Actions $actions): Actions
+    {
+        return $actions->remove(Crud::PAGE_INDEX, Action::NEW);
+    }
 }
