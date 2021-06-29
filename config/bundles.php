b655630aa2f3f65065d3834c94ae84a6d8c682b4
teebbstudios <qww.zone@gmail.com>
2021-06-29 19:01:10 +0800

    install twig-pack

--- config/bundles.php
+++ config/bundles.php
@@ -12,4 +12,5 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
 ];
