7d3f6f0b3ca068d9de9e88bba55cfcb7b9e1e69e
teebbstudios <qww.zone@gmail.com>
2021-06-29 17:58:51 +0800

    PostCrudController

--- src/Controller/Admin/PostCrudController.php
+++ src/Controller/Admin/PostCrudController.php
@@ -3,7 +3,17 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Post;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Filter\ChoiceFilter;
 
 class PostCrudController extends AbstractCrudController
 {
@@ -12,14 +22,36 @@ class PostCrudController extends AbstractCrudController
         return Post::class;
     }
 
-    /*
+
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
+            IdField::new('id')->onlyOnIndex(),
             TextField::new('title'),
-            TextEditorField::new('description'),
+            ImageField::new('postImage')
+                ->setBasePath('uploads/images')
+                ->setUploadDir('public/uploads/images')
+                ->setUploadedFileNamePattern('[slug]-[contenthash].[extension]'),
+            TextareaField::new('summary'),
+            TextEditorField::new('body'),
+            ChoiceField::new('status')
+                ->setChoices(fn() => ['draft' => 'draft', 'published' => 'published']),
+            TimeField::new('createdAt')
+                ->setFormat('Y-MM-dd HH:mm:ss')->onlyOnIndex(),
+            TimeField::new('updatedAt')
+                ->setFormat('Y-MM-dd HH:mm:ss')->onlyOnIndex(),
         ];
     }
-    */
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud->setDefaultSort(['id' => 'DESC'])
+            ->setSearchFields(['title', 'summary', 'body']);
+    }
+
+    public function configureFilters(Filters $filters): Filters
+    {
+        return $filters->add(ChoiceFilter::new('status')
+            ->setChoices(['draft' => 'draft', 'published' => 'published']));
+    }
 }
