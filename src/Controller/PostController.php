e3b36b5ac8868aade38a5aad40563d1158a8b60f
teebbstudios <qww.zone@gmail.com>
2021-06-30 14:55:09 +0800

    render() and fix index()

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -17,7 +17,7 @@ class PostController extends AbstractController
     public function index(PostRepository $postRepository): Response
     {
         return $this->render('post/index.html.twig', [
-            'posts' => $postRepository->findAll(),
+            'posts' => $postRepository->findBy(['status' => 'published'], ['id' => 'DESC']),
         ]);
     }
 
