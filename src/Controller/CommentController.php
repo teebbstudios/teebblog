1d7708de4a4435843fb6c2edc41b8d6d33a10836
teebbstudios <qww.zone@gmail.com>
2021-07-06 16:08:20 +0800

    macro show comment list

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Comment;
 use App\Entity\Post;
 use App\Form\CommentType;
+use Doctrine\ORM\EntityManagerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -18,11 +19,25 @@ class CommentController extends AbstractController
         ParamConverter('post', options: ['id' => 'post_id']),
         ParamConverter('parentComment', options: ['id' => 'comment_id']),
     ]
-    public function replyComment(Request $request, Post $post, Comment $parentComment): Response
+    public function replyComment(Request $request, Post $post, Comment $parentComment, EntityManagerInterface $em): Response
     {
         $replyComment = $this->createForm(CommentType::class, null, [
             'action' => $request->getUri()
         ]);
+
+        $replyComment->handleRequest($request);
+
+        if ($replyComment->isSubmitted() && $replyComment->isValid()){
+            /**@var Comment $data**/
+            $data = $replyComment->getData();
+            $data->setParent($parentComment);
+            $data->setPost($post);
+            $em->persist($data);
+            $em->flush();
+
+            return $this->redirectToRoute('post_show', ['id1' => $post->getId()]);
+        }
+
         return $this->render('comment/_reply_comment_form.html.twig', [
             'reply_comment_form' => $replyComment->createView(),
         ]);
