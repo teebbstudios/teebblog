0589060ea0bba14be355876bfaaf414fce2e7d04
teebbstudios <qww.zone@gmail.com>
2021-07-07 15:16:36 +0800

    install faker and PostFixtures

--- src/DataFixtures/PostFixtures.php
+++ src/DataFixtures/PostFixtures.php
@@ -5,6 +5,7 @@ namespace App\DataFixtures;
 use App\Factory\PostFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 
 class PostFixtures extends Fixture
 {
@@ -12,17 +13,26 @@ class PostFixtures extends Fixture
      * @var PostFactory
      */
     private $postFactory;
+    private $faker;
 
     public function __construct(PostFactory $postFactory)
     {
         $this->postFactory = $postFactory;
+        $this->faker=Factory::create();
     }
 
     public function load(ObjectManager $manager)
     {
-        for ($i = 0; $i < 5; $i++) {
-            $post = $this->postFactory->create('Fake Post Title ' . $i, 'Fake Post Body ' . $i);
-            if ($i %2 ==0){
+        for ($i = 0; $i < 20; $i++) {
+            $post = $this->postFactory->create($this->faker->sentence(), $this->faker->paragraph());
+            if ($this->faker->boolean()){
+                $post->setStatus('published');
+            }
+
+            $image = '00'.$this->faker->randomDigit().'.jpg';
+            $post->setPostImage($image);
+
+            if ($i == 19){
                 $post->setStatus('published');
             }
 
