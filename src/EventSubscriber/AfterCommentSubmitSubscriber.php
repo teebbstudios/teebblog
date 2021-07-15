81b4f77cc5b3d1216ee338c6878cc583c3451e76
teebbstudios <qww.zone@gmail.com>
2021-07-15 20:04:30 +0800

    74. AfterCommentSubmitSubscriber

---
+++ src/EventSubscriber/AfterCommentSubmitSubscriber.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use App\Event\AfterCommentSubmitEvent;
+
+class AfterCommentSubmitSubscriber implements EventSubscriberInterface
+{
+    /**
+     * @var ParameterBagInterface
+     */
+    private ParameterBagInterface $parameterBag;
+
+    public function __construct(ParameterBagInterface $parameterBag)
+    {
+        $this->parameterBag = $parameterBag;
+    }
+
+    public function onAfterCommentSubmitEvent(AfterCommentSubmitEvent $event)
+    {
+        $words = $this->parameterBag->get('strip_words');
+
+        $comment = $event->getComment();
+        $message = $comment->getMessage();
+
+        $comment->setMessage($this->stripWords($message, $words));
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            AfterCommentSubmitEvent::class => 'onAfterCommentSubmitEvent',
+        ];
+    }
+
+    private function stripWords(string $message, array $words)
+    {
+        foreach ($words as $word){
+            $message = str_replace($word, ' *** ', $message);
+        }
+
+        return $message;
+    }
+}
