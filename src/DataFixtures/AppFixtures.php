8b9cace2fdeb1aa4a25ad06c261b9599d20ed3ce
teebbstudios <qww.zone@gmail.com>
2021-07-16 19:40:54 +0800

    78. create admin user use AppFixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,31 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    /**
+     * @var UserPasswordHasherInterface
+     */
+    private UserPasswordHasherInterface $userPasswordHasher;
+
+    public function __construct(UserPasswordHasherInterface $userPasswordHasher)
+    {
+        $this->userPasswordHasher = $userPasswordHasher;
+    }
+
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $admin = new User();
+        $admin->setUsername('admin');
+        $admin->setRoles(['ROLE_SUPER_ADMIN']);
+        $admin->setPassword($this->userPasswordHasher->hashPassword($admin, 'admin'));
+
+        $manager->persist($admin);
 
         $manager->flush();
     }
