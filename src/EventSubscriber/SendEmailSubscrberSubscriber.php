5fbdbb684655ee6efde520121660e0f29ab95d79
伟伟权 <qww.zone@gmail.com>
2021-07-27 19:31:19 +0800

    105. MessageBus dispatch message and sync transport

--- src/EventSubscriber/SendEmailSubscrberSubscriber.php
+++ src/EventSubscriber/SendEmailSubscrberSubscriber.php
@@ -3,27 +3,39 @@
 namespace App\EventSubscriber;
 
 use App\Entity\Post;
+use App\Message\SendEmailMessage;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use \EasyCorp\Bundle\EasyAdminBundle\Event\AfterEntityPersistedEvent;
 use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Component\Mime\Email;
 
 class SendEmailSubscrberSubscriber implements EventSubscriberInterface
 {
-    /**
-     * @var ParameterBagInterface
-     */
-    private ParameterBagInterface $parameterBag;
-    /**
-     * @var MailerInterface
-     */
-    private MailerInterface $mailer;
+//    /**
+//     * @var ParameterBagInterface
+//     */
+//    private ParameterBagInterface $parameterBag;
+//    /**
+//     * @var MailerInterface
+//     */
+//    private MailerInterface $mailer;
+//
+//    public function __construct(ParameterBagInterface $parameterBag, MailerInterface $mailer)
+//    {
+//        $this->parameterBag = $parameterBag;
+//        $this->mailer = $mailer;
+//    }
 
-    public function __construct(ParameterBagInterface $parameterBag, MailerInterface $mailer)
+    /**
+     * @var MessageBusInterface
+     */
+    private MessageBusInterface $messageBus;
+
+    public function __construct(MessageBusInterface $messageBus)
     {
-        $this->parameterBag = $parameterBag;
-        $this->mailer = $mailer;
+        $this->messageBus = $messageBus;
     }
 
     public function onAfterEntityPersistedEvent(AfterEntityPersistedEvent $event)
@@ -31,13 +43,15 @@ class SendEmailSubscrberSubscriber implements EventSubscriberInterface
         $post = $event->getEntityInstance();
         if ($post instanceof  Post)
         {
-            $email = (new Email())
-                ->from($this->parameterBag->get('send_email'))
-                ->to($this->parameterBag->get('editor_email'), $this->parameterBag->get('checker_email'))
-                ->subject('有新的文章<'.$post->getTitle().'>发布了，请检查。')
-                ->text('有新的文章<'.$post->getTitle().'>发布了，请检查。');
-sleep(10);
-            $this->mailer->send($email);
+//            $email = (new Email())
+//                ->from($this->parameterBag->get('send_email'))
+//                ->to($this->parameterBag->get('editor_email'), $this->parameterBag->get('checker_email'))
+//                ->subject('有新的文章<'.$post->getTitle().'>发布了，请检查。')
+//                ->text('有新的文章<'.$post->getTitle().'>发布了，请检查。');
+//sleep(10);
+//            $this->mailer->send($email);
+            $message = new SendEmailMessage($post->getId());
+            $this->messageBus->dispatch($message);
         }
     }
 
