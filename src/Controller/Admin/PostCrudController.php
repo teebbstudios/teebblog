8ba36e88e0fe69e9b31092207cde92c7c846dc8d
伟伟权 <qww.zone@gmail.com>
2021-07-27 14:56:45 +0800

    101. workflow guard

--- src/Controller/Admin/PostCrudController.php
+++ src/Controller/Admin/PostCrudController.php
@@ -4,6 +4,7 @@ namespace App\Controller\Admin;
 
 use App\Entity\Post;
 use App\Security\Voter\PostVoter;
+use App\Utils\Transition;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
@@ -70,9 +71,26 @@ class PostCrudController extends AbstractCrudController
     public function configureActions(Actions $actions): Actions
     {
         $workflow = $this->container->get('workflow.blog_publishing');
-        $reviewRequestAction = Action::new('review_request', 'review_request')
-            ->displayIf(fn($entity) => $workflow->can($entity, 'review_request'))
+        $reviewRequestAction = Action::new(Transition::REVIEW_REQUEST, Transition::REVIEW_REQUEST)
+            ->displayIf(fn($entity) => $workflow->can($entity, Transition::REVIEW_REQUEST))
             ->linkToCrudAction('reviewRequestAction');
+
+        $editorReviewAction = Action::new(Transition::EDITOR_REVIEW, Transition::EDITOR_REVIEW)
+            ->displayIf(fn($entity) => $workflow->can($entity, Transition::EDITOR_REVIEW)
+//                && $this->isGranted('ROLE_EDITOR')
+            )
+            ->linkToCrudAction('editorReviewAction');
+
+        $checkerCheckAction = Action::new(Transition::CHECKER_CHECK, Transition::CHECKER_CHECK)
+            ->displayIf(fn($entity) => $workflow->can($entity, Transition::CHECKER_CHECK)
+//                && $this->isGranted('ROLE_CHECKER')
+            )
+            ->linkToCrudAction('checkerCheckAction');
+
+        $publishAction = Action::new(Transition::PUBLISH, Transition::PUBLISH)
+            ->displayIf(fn($entity) => $workflow->can($entity, Transition::PUBLISH))
+            ->linkToCrudAction('publishAction');
+
         return $actions->update(Crud::PAGE_INDEX, Action::EDIT,
             function (Action $action) {
                 return $action->displayIf(fn($entity) => $this->isGranted(PostVoter::POST_OWNER_EDIT, $entity));
@@ -80,27 +98,69 @@ class PostCrudController extends AbstractCrudController
             function (Action $action) {
                 return $action->displayIf(fn($entity) => $this->isGranted(PostVoter::POST_OWNER_DELETE, $entity));
             })
-            ->add(Crud::PAGE_INDEX, $reviewRequestAction);
+            ->add(Crud::PAGE_INDEX, $reviewRequestAction)
+            ->add(Crud::PAGE_INDEX, $editorReviewAction)
+            ->add(Crud::PAGE_INDEX, $checkerCheckAction)
+            ->add(Crud::PAGE_INDEX, $publishAction);
     }
 
     public function reviewRequestAction(AdminContext $adminContext)
     {
         $post = $adminContext->getEntity()->getInstance();
-        if ($post instanceof Post) {
+
+        $this->applyTransition($post, Transition::REVIEW_REQUEST);
+
+        return $this->redirect($this->generatePageUrl(PostCrudController::class, Action::INDEX));
+    }
+
+    public function editorReviewAction(AdminContext $adminContext)
+    {
+        $this->denyAccessUnlessGranted('ROLE_EDITOR');
+        $post = $adminContext->getEntity()->getInstance();
+
+        $this->applyTransition($post, Transition::EDITOR_REVIEW);
+
+        return $this->redirect($this->generatePageUrl(PostCrudController::class, Action::INDEX));
+    }
+
+    public function checkerCheckAction(AdminContext $adminContext)
+    {
+        $this->denyAccessUnlessGranted('ROLE_CHECKER');
+        $post = $adminContext->getEntity()->getInstance();
+
+        $this->applyTransition($post, Transition::CHECKER_CHECK);
+
+        return $this->redirect($this->generatePageUrl(PostCrudController::class, Action::INDEX));
+    }
+
+    public function publishAction(AdminContext $adminContext)
+    {
+        $post = $adminContext->getEntity()->getInstance();
+
+        $this->applyTransition($post, Transition::PUBLISH);
+
+        return $this->redirect($this->generatePageUrl(PostCrudController::class, Action::INDEX));
+    }
+
+    private function applyTransition($entity, $transitionName)
+    {
+        if ($entity instanceof Post) {
             $workflow = $this->container->get('workflow.blog_publishing');
-            if ($workflow->can($post, 'review_request')) {
-                $workflow->apply($post, 'review_request');
+            if ($workflow->can($entity, $transitionName)) {
+                $workflow->apply($entity, $transitionName);
                 $this->container->get('doctrine')->getManager()->flush();
             }
         }
+    }
 
+    private function generatePageUrl(string $crudControllerName, string $actionName)
+    {
         $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
 
-        return $this->redirect($adminUrlGenerator
-            ->setController(PostCrudController::class)
-            ->setAction(Action::INDEX)
-            ->generateUrl()
-        );
+        return $adminUrlGenerator
+            ->setController($crudControllerName)
+            ->setAction($actionName)
+            ->generateUrl();
     }
 
     public static function getSubscribedServices()
---
+++ src/Utils/Transition.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace App\Utils;
+
+
+class Transition
+{
+    public const REVIEW_REQUEST = 'review_request';
+    public const EDITOR_REVIEW = 'editor_review';
+    public const CHECKER_CHECK = 'checker_check';
+    public const PUBLISH = 'publish';
+}
