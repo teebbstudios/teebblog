379c5e917be81710c92b8f463c8df67c71d460d1
伟伟权 <qww.zone@gmail.com>
2022-02-23 19:46:39 +0800

    bundle-14.在Bundle中创建事件，使用事件为文件类的属性赋值

--- src/Entity/SimpleFile.php
+++ src/Entity/SimpleFile.php
@@ -19,8 +19,25 @@ class SimpleFile extends File
      */
     private $id;
 
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $path;
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getPath(): ?string
+    {
+        return $this->path;
+    }
+
+    public function setPath(string $path): self
+    {
+        $this->path = $path;
+
+        return $this;
     }
 }
---
+++ src/EventSubscriber/SimpleFileSubscriber.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use App\Entity\SimpleFile;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use \Teebb\UploaderBundle\Event\AfterFileObjectSetPropertyEvent;
+
+class SimpleFileSubscriber implements EventSubscriberInterface
+{
+    public function onAfterFileObjectSetPropertyEvent(AfterFileObjectSetPropertyEvent $event)
+    {
+        /**@var UploadedFile $uploadedFile * */
+        $uploadedFile = $event->getUploadedFile();
+        /**@var SimpleFile $simpleFile * */
+        $simpleFile = $event->getFileObject();
+
+        $simpleFile->setPath('teebb_uploader' . \DIRECTORY_SEPARATOR. $simpleFile->getFileName() );
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            AfterFileObjectSetPropertyEvent::class => 'onAfterFileObjectSetPropertyEvent',
+        ];
+    }
+}
